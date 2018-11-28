844ce77845b012733e9f21a78319c2000bdbe946
Martynas <packmaster@riawolf.com>
2018-11-28 11:38:29 +0200

    uzdavinys 15

--- task15/script.js
+++ task15/script.js
@@ -3,7 +3,7 @@ function handleClick() {
     var mas = text.split("");
     var kiekis = 0;
     mas.forEach(function (t) {
-        if (t = "a") kiekis++;
+        if (t == "a") kiekis++;
     });
 
     document.getElementById("output").innerHTML = "Zodyje " + text + " a raidziu surasta : " + kiekis;
